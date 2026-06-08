357c52959cebafee2cebcfbe247a3aeb397adfae
foxhound11 <181505439+foxhound11@users.noreply.github.com>
2026-06-08 19:29:36 +0100

    Add Discord redirect

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/S8UUehY4d",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
